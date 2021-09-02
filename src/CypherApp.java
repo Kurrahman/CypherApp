6ae5dc098f82ed7336d940a2c31f9a8299260fae
Kurrahman <13517074@std.stei.itb.ac.id>
2021-09-03 01:16:54 +0700

    add main class

---
+++ src/CypherApp.java
@@ -0,0 +1,8 @@
+import Interface.Interface;
+
+public class CypherApp {
+    public static void main(String[] args) {
+        Interface display = new Interface();
+        display.initInterface();
+    }
+}
