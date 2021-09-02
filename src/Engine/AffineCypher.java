181fa700c3fcd928cf1d7479e5fed08a3c3365af
Kurrahman <13517074@std.stei.itb.ac.id>
2021-09-02 17:00:03 +0700

    add AffineCypher

---
+++ src/Engine/AffineCypher.java
@@ -0,0 +1,64 @@
+package Engine;
+
+import Utils.Utils;
+
+import java.util.HashMap;
+
+public class AffineCypher {
+    private static HashMap<Character, Character> encryptMap = new HashMap<>(), decryptMap = new HashMap<>();
+    private static final Character[] ALPHABET = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+    public static void generateCypherMap(int a, int b){
+           encryptMap.clear();
+           decryptMap.clear();
+
+        for (Character c: ALPHABET) {
+            int x = Utils.charToInt(c);
+            int y = (a*x+b) % 26;
+            char tmp = Utils.intToChar(y);
+            encryptMap.put(c,tmp);
+            decryptMap.put(tmp,c);
+        }
+    }
+
+    public static String encrypt(String plain, int a, int b){
+        StringBuilder out = new StringBuilder();
+        generateCypherMap(a,b);
+        char[] upPlain = plain.toUpperCase().trim().toCharArray();
+
+        for (char c: upPlain) {
+            if (Utils.isAlphabet(c)){
+                out.append(encryptMap.get(c));
+            } else {
+                out.append(c);
+            }
+        }
+
+        return out.toString().trim();
+    }
+
+    public static String decrypt(String cypher, int a, int b){
+        StringBuilder out = new StringBuilder();
+        generateCypherMap(a,b);
+        char[] upCypher = cypher.toUpperCase().trim().toCharArray();
+
+        for (char c: upCypher) {
+            if (Utils.isAlphabet(c)){
+                out.append(decryptMap.get(c));
+            } else {
+                out.append(c);
+            }
+        }
+
+        return out.toString().trim();
+    }
+
+
+
+    public static void main(String[] args) {
+        String plain = "kripto";
+        String cypher = AffineCypher.encrypt(plain,7,10);
+        System.out.println(cypher);
+        System.out.println(AffineCypher.decrypt(cypher,7,10));
+    }
+}
---
+++ src/Interface/Affine.java
@@ -0,0 +1,60 @@
+package Interface;
+
+import Engine.AffineCypher;
+import Engine.PlayFairCypher;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Affine extends Template implements ActionListener {
+    private JComboBox<Integer> coprime;
+    private JTextField shift;
+    private Integer[] coprimeList = {1, 3, 5, 7, 9,11,13,15,17,19,21,23,25};
+
+    public void displayAffine(JFrame frame){
+        displayBase(frame);
+
+        JLabel coprimeLabel = new JLabel("Coprime : ");
+        coprimeLabel.setBounds(350, 110, 80, 20);
+        frame.add(coprimeLabel);
+
+        coprime = new JComboBox<>(coprimeList);
+        coprime.setBounds(350, 130, 80, 30);
+        coprime.setSelectedIndex(0);
+        coprime.addActionListener(this);
+        frame.add(coprime);
+
+        JLabel shiftLabel = new JLabel("Shift : ");
+        shiftLabel.setBounds(350, 170, 80, 20);
+        frame.add(shiftLabel);
+
+        shift = new JTextField();
+        shift.setBounds(350,190,80,30);
+        shift.setBorder(textArea);
+        frame.add(shift);
+
+        frame.setVisible(false);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == decryptBtn) {
+            plain.setText(AffineCypher.decrypt(cypher.getText(), (int) coprime.getSelectedItem(), Integer.parseInt(shift.getText())));
+        }
+        if (e.getSource() == encryptBtn) {
+            cypher.setText(AffineCypher.encrypt(plain.getText(), (int) coprime.getSelectedItem(), Integer.parseInt(shift.getText())));
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("test");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        Affine affine = new Affine();
+        affine.displayAffine(frame);
+    }
+}
